36595494edbe8e6ac36d0780e90b4e7d7f48bee8
Maciej <79467814+maciej-j-szulc@users.noreply.github.com>
2025-02-19 11:34:41 +0100

    Lazy Loading implementation

--- src/router.js
+++ src/router.js
@@ -1,13 +1,33 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+//import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachesRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+//import CoachesRegistration from './pages/coaches/CoachRegistration.vue';
+//import ContactCoach from './pages/requests/ContactCoach.vue';
+//import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
+//import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index';
+
+const CoachDetail = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachDetail.vue')
+);
+const CoachesRegistration = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachRegistration.vue')
+);
+
+const ContactCoach = defineAsyncComponent(() =>
+  import('./pages/requests/ContactCoach.vue')
+);
+
+const RequestsReceived = defineAsyncComponent(() =>
+  import('./pages/requests/RequestsReceived.vue')
+);
+
+const UserAuth = defineAsyncComponent(() =>
+  import('./pages/auth/UserAuth.vue')
+);
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,25 +38,31 @@ const router = createRouter({
       path: '/coaches/:id',
       component: CoachDetail,
       props: true,
-      children: [
-        { path: 'contact', component: ContactCoach }
-    ],
+      children: [{ path: 'contact', component: ContactCoach }],
     },
-    { path: '/register', component: CoachesRegistration, meta: { requiresAuth: true} },
-    { path: '/requests', component: RequestsReceived,meta: { requiresAuth: true} },
-    {path: '/auth', component: UserAuth, meta: { requiresUnauth: true}},
+    {
+      path: '/register',
+      component: CoachesRegistration,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
-router.beforeEach(function(to, _, next) {
-  if(to.meta.requiresAuth && !store.getters.isAuthenticated){
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
     next('/auth');
-  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated){
-    next('/coaches')
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/coaches');
   } else {
     next();
   }
-})
+});
 
 export default router;
